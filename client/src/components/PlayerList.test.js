3642a2c97800c8f72e1fff0308637d9507e54c70
Kristen Poe <kristen@Kristens-iMac.local>
2019-12-15 22:15:44 -0500

    second commit

---
+++ client/src/components/PlayerList.test.js
@@ -0,0 +1,10 @@
+describe('player list component', () => {
+    it('renders without crashing', () => {
+    //    functionsum(sum(2,2).toBe(4))
+
+    const sum = (a,b) => {
+        return a+b;
+    }
+    expect(sum(2,2)).toBe(4)
+    })
+})
